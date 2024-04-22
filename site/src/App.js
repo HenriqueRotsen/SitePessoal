95b245349b4e26a8f56c084fda61c8c36bc8d709
HenriqueRotsen <henriquerotsensf@gmail.com>
2024-04-22 14:25:21 -0300

    Adição de rodapé na página e favicon

--- site/src/App.js
+++ site/src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header.js';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import { Home } from './pages/Home.js'
 import { Curriculo } from './pages/Curriculo.js'
+import Footer from './components/Footer.js'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" exact Component={Home}/>
         <Route path="/curriculo" Component={Curriculo}/>
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
---
+++ site/src/components/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { getYear } from "https://unpkg.com/date-fns/getYear.mjs";
+import '../styles/Footer.css'
+
+
+function Footer() {
+  const anoAtual = getYear(new Date());
+
+  return (
+    <>
+      <div className='footer-container'>
+        <div className='social-medias'>
+          <a href='https://www.linkedin.com/in/henrique-rotsen-santos-ferreira/'>
+            <i class="fab fa-linkedin"/>
+          </a>
+          <a href='https://www.facebook.com/profile.php?id=100002146099863'>
+            <i class="fab fa-facebook" />
+          </a>
+          <a href='https://github.com/HenriqueRotsen'>
+            <i class="fab fa-github" />
+          </a>
+        </div>
+        <div className='copyright'>
+          Henrique Rotsen - {anoAtual} Todos os direitos reservados Ⓡ
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
